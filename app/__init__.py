78d112b3bc9ddfbc57057c582cfd4b1a3669c6d8
Chanderpaul Martin <chanderpaul.martin@mymona.uwi.edu>
2018-02-26 18:20:37 +0000

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -8,5 +8,8 @@ PASSWORD = 'password123'
 SECRET_KEY = 'Sup3r$3cretkey'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './app/static/uploads'
+allowed_uploads = ['png', 'jpg']
+
 app.config.from_object(__name__)
 from app import views
